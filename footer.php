e85656eac47cf4a29ef8df7724b8cd9f66c62ec4
cortell <dev@cortelldavis.com>
2015-02-24 21:30:55 -0800

    Initial commit

---
+++ footer.php
@@ -0,0 +1,11 @@
+
+ </body>
+
+<footer>
+  
+</footer>
+
+</html>
+
+<script src="assets/js/jquery-2.1.3.min.js"></script>
+<script src="assets/js/bootstrap-3.3.2.min.js"></script>
---
+++ header.php
@@ -0,0 +1,30 @@
+<!--PRIVATE LICENSE-->
+
+<html>
+
+<head></head>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>Theme Template for Bootstrap</title>
+
+    <!-- Font Awesome Icon Set -->
+    <link rel="stylesheet" href="assets/fonts/font-awesome-4.3.0/css/font-awesome.min.css">
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap-3.3.2.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/theme.css" rel="stylesheet">
+
+  </head>
+
+
+  <body role="document">
